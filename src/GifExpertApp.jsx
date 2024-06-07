294daa7b8451a1fbf7ee0a25497fd7e62cc20ffc
Cloudxz1 <cloudstrife1877@gmail.com>
2024-06-06 21:07:13 -0400

    Add GifItem, GifGrid and styles.css

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One punch']);
+
+    // Agregar una nueva categoria
+    const onAddCategory = ( newCategory ) => {
+        if(categories.includes(newCategory)) return;
+        
+        setCategories([newCategory, ...categories ]);
+    };
 
     return( 
         <>
+            {/* Titulo */}
             <h1>GifExpertApp</h1>
+            {/* input */}
+            <AddCategory 
+                // setCategories={ setCategories }
+                onNewCategory = { event => onAddCategory(event) }
+            />
+            {/* Listado de gifs */}
+            {/* <button onClick={onAddCategory}>Agregar</button> */}
+            
+            {
+                categories.map(category =>(
+                <GifGrid key ={category} 
+                category={ category }
+                />))
+            }
+            
+            {/* GifsItem */}
         </>
     )
 };
---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,28 @@
+import { getGifs } from "../helpers/getGifs";
+import { useEffect, useState } from "react";
+import { GifItem } from "./GifItem";
+export const GifGrid = ({category}) =>{
+    
+    const [images,setImages] = useState([]);
+
+    useEffect(()=>{
+        getGifs(category)
+        .then(newImages => setImages(newImages));
+    },[]);
+    
+    return(
+        <>
+            <h3>{category}</h3>
+            <div className="card-grid">
+                {
+                    images.map(( image ) => (
+                        <GifItem 
+                            key={ image.id }
+                            {... image }
+                        />
+                    ))
+                }
+            </div>
+        </>
+    )
+}
